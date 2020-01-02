0c3711f1ca9cd15ff5fb50ea1dd301e71e981136
Ron <rbejerano@ucvts.org>
2020-01-02 10:37:03 -0500

    inventory to be used throughout mission

---
+++ Code Blocks/inventory.py
@@ -0,0 +1,26 @@
+class Inventory:
+    """Creating player's inventory for their mission"""
+
+    tools = {
+        'binoculars': {
+            'quantity': 1,
+            'uses': ['zoom_in', 'night_vision', 'thermal_vision']
+        },
+        'bug' :  {
+            'quantity': 3,
+            'uses': ['eavesdrop', 'record']
+        },
+        'camera':  {
+            'quantity': 1,
+            'uses': ['zoom_in', 'taking_pictures', 'image_filtering', 'image_to_chip']
+        },
+        'flashdrive':  {
+            'quantity': 1,
+            'uses': ['storage']
+        },
+        'phone':  {
+            'quantity': 1,
+            'uses': ['receive_instructions', 'erase', 'tracking', 'self_destruct']
+        }
+    }
+
