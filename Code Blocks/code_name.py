07a3e2fc309e9c1bdb4b3531c043dc9d1d6e52de
Ron <rbejerano@ucvts.org>
2020-01-02 09:41:27 -0500

    Commit of code name chain.

---
+++ Code Blocks/code_name.py
@@ -0,0 +1,8 @@
+#Plan: Create a Code Name Function that takes an input and returns a code name.
+
+def code_name():
+    """Takes the name input from user and returns code name"""
+
+    first_word = ["anaconda",]
+    
+    original_name = input("Before we begin, chief needs your name: ")
