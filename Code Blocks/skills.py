7ebc92d8da7099adffbababacc0dcc7d18434909
Ron <rbejerano@ucvts.org>
2020-01-06 09:51:48 -0500

    skills bank

---
+++ Code Blocks/skills.py
@@ -0,0 +1,8 @@
+class Skills:
+    """A bank of skills that player will collect on mission"""
+
+    skills: {
+        'espionage': ['follow', 'record', 'plant'],
+        'technology': ['decrypt', 'encrypt', 'storage'],
+        'combat': ['hand_to_hand', 'firearm']
+    }
