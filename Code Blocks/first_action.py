20f3b322bb872dc2faa426cf6cc67fdc32bd6cd3
Ron <rbejerano@ucvts.org>
2020-01-08 10:48:22 -0500

    Updated Version

--- Code Blocks/first_action.py
+++ Code Blocks/first_action.py
@@ -5,7 +5,7 @@ import random
 import sys
 
 #Classes
-class Inventory:
+class Inventory():
     """Creating player's inventory for their mission"""
 
     tools = {
@@ -30,7 +30,7 @@ class Inventory:
             'uses': ['receive_instructions', 'erase', 'tracking', 'self_destruct']
         }
     }
-class Skills:
+class Skills():
     """A bank of skills that player will collect on mission"""
 
     skills: {
@@ -40,10 +40,37 @@ class Skills:
     }
 
 
-def Reports(report):
+def Packages(package):
     """Opens and sends messages about reports"""
 
-    print("You have received a report.")
-    open_report = input("Would you like to open it? ").lower()
+    print("You have received a package.")
+    open_package = input("Would you like to open it? ").lower()
 
-    if open_report = 'yes'
+    if open_package == 'yes':
+        print(package)
+    else:
+        print("The package will be safely dealt with.")
+
+def Commands(command):
+    """Function for receiving commands"""
+
+    print("You have received a letter from HQ.")
+    print(command)
+
+Commands(CommA)    
+
+CommA = {
+    print(f"Agent {code}.")
+    print(f"    Welcome to Translutia, more specifically, the biggest port city of Hagenstade.")
+    print(f"    This city is the most democratic in the country, and as a result is under strict supervision under the dictatorship.")
+    print(f"    This city is crawling with spies and counterspies. Be of utmost precaution.")        
+    print(f"    The city will be hosting its annual celebratory festival in honor of dictator Bashmany Rotendero.")
+    print(f"    We need you to scout the parade that will be held there. ")
+    print(f"    The parade begins in Hagenstade, and ends at the capital, Rotendale. The senior team will then infiltrate the dictator's compound.")
+    print(f"    You are there to make sure no one sabotages the parade, and intervenes in our plans. ")
+    print(f"    The senior team will arrive in two days. The parade will be the day after.")
+    print(f"    We will be sending you some packages as well. These packages contain valuable tools and information.")
+    print(f"    However, we urge you to be wary. The dictator most surely knows you are in the country, and they will be trying to put a stop to the mission.")
+    print(f"    Be suspicious of everything. You will know something is ours if we mention it beforehand, or a decrypted message reads GSS.")
+    print(f"    Good luck agent. Your first package is at the back left door of the train station.")
+}
