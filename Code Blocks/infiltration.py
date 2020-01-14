30f2eda8a6af5fb555e36944cbbb9aa8d53aa56b
Ron <rbejerano@ucvts.org>
2020-01-14 10:42:32 -0500

    New Code Block

---
+++ Code Blocks/infiltration.py
@@ -0,0 +1,12 @@
+codes = {
+    'door': ['compound1', 'compound2'],
+
+}
+
+def door_unlocking():
+    """Function that unlocks doors"""
+
+    check_code = codes['door'][0]
+    print(check_code)
+
+door_unlocking()
