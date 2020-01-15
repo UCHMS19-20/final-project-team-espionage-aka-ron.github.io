23bd3258e33b9ca9eb57c9a1621100e98e030b1b
Ron <rbejerano@ucvts.org>
2020-01-15 13:22:32 -0500

    Updates

--- Code Blocks/infiltration.py
+++ Code Blocks/infiltration.py
@@ -1,3 +1,5 @@
+import pygame
+
 codes = {
     'door': ['compound1', 'compound2'],
 
@@ -6,7 +8,19 @@ codes = {
 def door_unlocking():
     """Function that unlocks doors"""
 
+    print("You are approaching a door.")
+
     check_code = codes['door'][0]
-    print(check_code)
+    if check_code == 'compound1':
+        print("It seems to be a strong metal door.") 
+        print("There is a 4x4 keypad on the side with a wide assortment of symbols, letters, and numbers.")
+        print("Maybe the decoder will help...")
+        
+    elif check_code == 'compound2':
+        hrkjh
+    elif check_code == 'compound3':
+        hkjg
+    else:
+        print("You do not have the correct decoder to try this door. Find the encryption and try again.")
 
 door_unlocking()
