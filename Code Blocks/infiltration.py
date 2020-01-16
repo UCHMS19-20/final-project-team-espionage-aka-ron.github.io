af90f933278097b2b07f01870b2887a047ddf499
Ron <rbejerano@ucvts.org>
2020-01-16 09:50:51 -0500

    Precaution

--- Code Blocks/infiltration.py
+++ Code Blocks/infiltration.py
@@ -1,4 +1,14 @@
 import pygame
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode( (1000, 1000) )
+
+font = pygame.font.Font("CourierM.ttf", 250)
+text = font.render("Letters", True, (200, 200, 200))
+xpos = 50
+ypos = 50
 
 codes = {
     'door': ['compound1', 'compound2'],
@@ -16,6 +26,12 @@ def door_unlocking():
         print("There is a 4x4 keypad on the side with a wide assortment of symbols, letters, and numbers.")
         print("Maybe the decoder will help...")
         
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            screen.blit(text, (xpos, ypos))
+            pygame.display.flip()
     elif check_code == 'compound2':
         hrkjh
     elif check_code == 'compound3':
