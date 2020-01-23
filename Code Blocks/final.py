b90c00994daada8d5e892516b71eb13e53fdde5a
Ron <rbejerano@ucvts.org>
2020-01-23 08:16:20 -0500

    Finished code for demo

--- Code Blocks/final.py
+++ Code Blocks/final.py
@@ -105,18 +105,18 @@ def code_name():
     }
 
     print("Now just to give you a code name. We can't risk any foreign interceptions revealing your assignment.")
-    ##(2.5)
+    time.sleep(2.5)
     original_name = input("I'm going to need your real name before we get started: ").lower()
     first_letter = original_name[0]
     last_letter = original_name[-1]
     
     code = first_word[first_letter] + " " + second_word[last_letter]  
     
-    ##(3.5)
+    time.sleep(3.5)
     print("Working...")
-    ##(3.5)
+    time.sleep(3.5)
     print(f"Alright, from now on, you will be known as {code} or {first_letter.lower()}_{last_letter.upper()} for short.")
-    ##(1.5)
+    time.sleep(1.5)
     print("Now you're ready to go.")
 
     return code
@@ -761,41 +761,41 @@ def locations(hour_star_choice_input):
 def main():
     """Introductory Procedure"""
 
-    ##(3.0)
+    time.sleep(3.0)
     print("")
     print("")
     print("Welcome to the Egg, the headquarters of the GSS")
-    ##(1.5)
+    time.sleep(1.5)
     print("The CIA sent you here to aid in our hunt of the corrupt Prime Minister of Translutia, who the FBI have labelled as a national security threat of the highest order.")
-    ##(3)
+    time.sleep(3)
     print("You have shown good discipline in the field, and you could be a great asset to us.")
-    ##(1.5)
+    time.sleep(1.5)
     agent_name = code_name()
-    ##(1.5)
+    time.sleep(1.5)
     print("You've been given some basic tools to help you in addition to your own skillset.")
-    ##(1.5)
+    time.sleep(1.5)
     print("You'll be going in to scout the area before we send in the senior team.")
-    ##(1.5)
+    time.sleep(1.5)
     print("You are to stay silent and report ONLY. We don't want to cause an international crisis here.")
-    ##(1.5)
+    time.sleep(1.5)
     print("I will be helping you along the way, making sure you're on the right track.")
-    ##(1.5)
+    time.sleep(1.5)
     print("Headquarters will also send you some commands, which you will receive on your phone. Don't lose it.")
-    ##(1.5)
+    time.sleep(1.5)
     print("Expect your first command at exactly 08:00. Make sure you read it in secret.")
-    ##(1.5)
+    time.sleep(1.5)
     print("With that all said and done, your train to Translutia leaves in 20 minutes. Don't miss it.")
-    ##(2)
+    time.sleep(2)
     print("")
-    ##(0.75)
+    time.sleep(0.75)
     print("")
-    ##(0.75)
+    time.sleep(0.75)
     print("                                         (▀̿Ĺ̯▀̿ ̿)                                              ")
-    ##(0.75)
+    time.sleep(0.75)
     print("")
-    ##(0.75)
+    time.sleep(0.75)
     print("")
-    ##(0.75)
+    time.sleep(0.75)
 
     #First Action Variables
 
